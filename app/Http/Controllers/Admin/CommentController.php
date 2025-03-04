d701dae86ff6a4e01399c1c63ff4ed6066265e55
dinhvanly <lydvph47356@fpt.edu.vn>
2025-03-04 16:12:42 +0700

    add_comment
    lý code

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Dotenv\Util\Str;
 use Illuminate\Http\Request;
 use App\Models\Comment;
+
 class CommentController extends Controller
 {
    public function index() {
@@ -20,5 +23,29 @@ class CommentController extends Controller
     $comment->save();
 
     return redirect()->back()->with('success', 'Trạng thái bình luận đã được cập nhật');
+   }
+
+   public function create(){
+      $blogs = Blog::all();
+      // $comment = comment::all();
+      $title = "Add comment";
+      return view('admin.comment.add',compact ('title','blogs'));
+
+   }
+
+   public function store(Request $request){
+   $validated = $request->validate([
+         'name' => 'required|string|max:255',
+         'comment' => 'required|string',
+      ]);
+ 
+    $comment =  comment::create([
+      'name'=>$validated['name'],
+      'comment'=>$validated['comment'],
+
+      ]);
+
+      return redirect()->route('admin.comment')->with('success', 'thêm thành công');
+
    }
 }
