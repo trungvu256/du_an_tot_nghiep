68c75c8251bd46a39fac8fa51ff0a860f53460e3
trongvnph47351 <vuongngoctrong1703@gmail.com>
2025-03-05 13:49:08 +0700

    đổ giao diện
    giao diện sản phẩm

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     //
     public function index(){
-        return view('web2.Home.home');
+        $list_product = Product::with('category')->paginate(8);
+        $list_product_new = Product::with('category')->orderBy('created_at', 'desc')->get();
+
+        return view('web2.Home.home',compact('list_product','list_product_new'));
     }
     public function shop(){
         return view('web2.Home.shop');
