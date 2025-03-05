dcb936232499ff243da7ce92c35b7d6e795b735d
taducuy2002 <taducuy123456@gmail.com>
2025-03-05 18:51:41 +0700

    code_xoa_mem
    code xóa mềm product

--- app/Models/Images.php
+++ app/Models/Images.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Images extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
-    protected $fillable = ['product_id', 'image']; // Thêm các cột cần thiết
+    protected $dates = ['delete_at'];
+
+    protected $fillable = ['product_id', 'image'];
 }
