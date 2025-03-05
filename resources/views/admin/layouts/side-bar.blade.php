a66ac43b5795c681f9cdff90d8f888da3db970c3
trongvnph47351 <vuongngoctrong1703@gmail.com>
2025-03-05 15:02:12 +0700

    giaodien_admin_v1

--- resources/views/admin/layouts/side-bar.blade.php
+++ resources/views/admin/layouts/side-bar.blade.php
@@ -1,26 +1,19 @@
 <div class="app-menu navbar-menu">
-    <!-- LOGO -->
     <div class="navbar-brand-box">
-        <!-- Dark Logo-->
         <a href="index.html" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="{{ asset('template/admin/velzon/assets/images/logo-sm.png') }}" alt=""
-                    height="22">
+                <img src="{{ asset('template/admin/velzon/assets/images/logo-sm.png') }}" alt="" height="22">
             </span>
             <span class="logo-lg">
-                <img src="{{ asset('template/admin/velzon/assets/images/logo-dark.png') }}" alt=""
-                    height="17">
+                <img src="{{ asset('template/admin/velzon/assets/images/logo-dark.png') }}" alt="" height="17">
             </span>
         </a>
-        <!-- Light Logo-->
         <a href="index.html" class="logo logo-light">
             <span class="logo-sm">
-                <img src="{{ asset('template/admin/velzon/assets/images/logo-sm.png') }}" alt=""
-                    height="22">
+                <img src="{{ asset('template/admin/velzon/assets/images/logo-sm.png') }}" alt="" height="22">
             </span>
             <span class="logo-lg">
-                <img src="{{ asset('template/admin/velzon/assets/images/logo-light.png') }}" alt=""
-                    height="17">
+                <img src="{{ asset('template/admin/velzon/assets/images/logo-light.png') }}" alt="" height="17">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
@@ -31,107 +24,96 @@
 
     <div id="scrollbar">
         <div class="container-fluid">
-
-            <div id="two-column-menu">
-            </div>
+            <div id="two-column-menu"></div>
             <ul class="navbar-nav" id="navbar-nav">
                 <li class="menu-title"><span data-key="t-menu">Menu</span></li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse"
-                        role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Category</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('admin.create.cate') }}" class="nav-link" data-key="t-analytics">
-                                    Add Category </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('admin.cate') }}" class="nav-link" data-key="t-crm">
-                                    Category </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li> <!-- end Dashboard Menu -->
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse"
-                        role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Product</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('admin.add.product') }}" class="nav-link" data-key="t-analytics">
-                                    Add product </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('admin.product') }}" class="nav-link" data-key="t-crm">
-                                    Product </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li> <!-- end Dashboard Menu -->
 
+                <!-- User -->
                 <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse"
-                        role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Comment</span>
+                    <a class="nav-link menu-link" href="#menuUser" data-bs-toggle="collapse" role="button">
+                        <i class="ri-user-line"></i> <span>User</span>
                     </a>
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
+                    <div class="collapse menu-dropdown" id="menuUser">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a href="{{ route('create.comment') }}" class="nav-link" data-key="t-analytics">
-                                    Add comment </a>
+                                <a href="{{ route('admin.create.user') }}" class="nav-link">Add User</a>
                             </li>
                             <li class="nav-item">
-                                <a href="{{ route('admin.comment') }}" class="nav-link" data-key="t-crm">
-                                    Comment </a>
+                                <a href="{{ route('admin.user') }}" class="nav-link">User</a>
                             </li>
                         </ul>
                     </div>
-                </li> <!-- end Dashboard Menu -->
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse"
-                        role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Blog</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('admin.create.blog') }}" class="nav-link" data-key="t-analytics">
-                                    Add blog </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('admin.blog') }}" class="nav-link" data-key="t-crm">
-                                    Blog </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li> <!-- end Dashboard Menu -->
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse"
-                        role="button" aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">User</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('admin.create.user') }}" class="nav-link" data-key="t-analytics">
-                                    Add user </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('admin.user') }}" class="nav-link" data-key="t-crm">
-                                    User </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li> <!-- end Dashboard Menu -->
+                </li>
 
+                <!-- Category -->
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#menuCategory" data-bs-toggle="collapse" role="button">
+                        <i class="ri-dashboard-2-line"></i> <span>Category</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="menuCategory">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.create.cate') }}" class="nav-link">Add Category</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin.cate') }}" class="nav-link">Category</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Product -->
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#menuProduct" data-bs-toggle="collapse" role="button">
+                        <i class="ri-shopping-cart-line"></i> <span>Product</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="menuProduct">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.add.product') }}" class="nav-link">Add Product</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin.product') }}" class="nav-link">Product</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Comment -->
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#menuComment" data-bs-toggle="collapse" role="button">
+                        <i class="ri-chat-3-line"></i> <span>Comment</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="menuComment">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="{{ route('create.comment') }}" class="nav-link">Add Comment</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin.comment') }}" class="nav-link">Comment</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Blog -->
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#menuBlog" data-bs-toggle="collapse" role="button">
+                        <i class="ri-file-list-line"></i> <span>Blog</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="menuBlog">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="{{ route('admin.create.blog') }}" class="nav-link">Add Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('admin.blog') }}" class="nav-link">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
             </ul>
         </div>
-        <!-- Sidebar -->
     </div>
-
     <div class="sidebar-background"></div>
 </div>
