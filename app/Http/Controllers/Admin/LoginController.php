eb87928995a503ecf18232e021adeeb5e75f183b
duchv120800 <duchv1208@gmail.com>
2025-03-06 13:38:01 +0700

    edit admin login

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -12,17 +12,21 @@ class LoginController extends Controller
     {
         return view('admin.login.index');
     }
-   public function loginAdmin(Request $request) {
-       $request->validate([
-           'email'=> 'required|email',
-           'password'=>'required|min:6',
-       ]);
-       if(Auth::attempt(['email' => $request->email, 'password' => $request->password],$request->remenber)) {
+    public function loginAdmin(Request $request) {
+        $request->validate([
+            'email'=> 'required|email',
+            'password'=>'required|min:6',
+        ]);
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+            'is_admin' => 1
+        ], $request->remember)) {
             return redirect()->route('admin.main');
-       } else {
-           return back()->with('error','Email or Password is not corrected');
-       }
-   }
+        } else {
+            return back()->with('error', 'Email or Password is incorrect, or you do not have admin access.');
+        }
+    }
    public function logout() {
        Auth::logout();
        return redirect()->route('login');
