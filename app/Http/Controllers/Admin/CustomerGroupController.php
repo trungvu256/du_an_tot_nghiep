5fc06dc2615c18ca3752675f7f38d2c5a680764c
dinhvanly <lydvph47356@fpt.edu.vn>
2025-03-18 12:59:33 +0700

    editnhom

--- app/Http/Controllers/Admin/CustomerGroupController.php
+++ app/Http/Controllers/Admin/CustomerGroupController.php
@@ -44,4 +44,27 @@ class CustomerGroupController extends Controller
         // Chuyển hướng trở lại trang danh sách nhóm khách hàng với thông báo thành công
         return redirect()->route('customer.index')->with('success', 'Nhóm khách hàng đã được thêm thành công!');
     }
+
+    // Hiển thị form chỉnh sửa nhóm khách hàng
+    public function edit(CustomerGroup $group)
+    {
+        return view('admin.customer.edit', compact('group'));
+    }
+
+    // Cập nhật nhóm khách hàng
+    public function update(Request $request, CustomerGroup $group)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'min_order_value' => 'required|numeric',
+            'min_order_count' => 'required|integer',
+            'description' => 'nullable|string',
+        ]);
+
+        $group->update($request->all()); // Cập nhật nhóm khách hàng
+
+        return redirect()->route('customer.index')->with('success', 'Nhóm khách hàng đã được cập nhật!');
+    }
+
+    
 }
