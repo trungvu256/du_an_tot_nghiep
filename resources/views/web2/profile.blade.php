492f28d9ba0d4468629b0a1a01bd545c2043263c
trongvnph47351 <vuongngoctrong1703@gmail.com>
2025-03-19 19:29:12 +0700

    giaodien_edit_user

--- resources/views/web2/profile.blade.php
+++ resources/views/web2/profile.blade.php
@@ -1,60 +1,7 @@
 @extends('web2.layout.master')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row border-top px-xl-5">
-        <div class="col-lg-3 d-none d-lg-block">
-            <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100"
-                data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
-                <h6 class="m-0">Categories</h6>
-                <i class="fa fa-angle-down text-dark"></i>
-            </a>
-            <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light"
-                id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
-                <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link" data-toggle="dropdown">Dresses <i
-                                class="fa fa-angle-down float-right mt-1"></i></a>
-                        <div class="dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0">
-                            <a href="" class="dropdown-item">Men's Dresses</a>
-                            <a href="" class="dropdown-item">Women's Dresses</a>
-                            <a href="" class="dropdown-item">Baby's Dresses</a>
-                        </div>
-                    </div>
-                    <a href="" class="nav-item nav-link">Shirts</a>
-                    <a href="" class="nav-item nav-link">Jeans</a>
-                    <a href="" class="nav-item nav-link">Swimwear</a>
-                    <a href="" class="nav-item nav-link">Sleepwear</a>
-                    <a href="" class="nav-item nav-link">Sportswear</a>
-                    <a href="" class="nav-item nav-link">Jumpsuits</a>
-                    <a href="" class="nav-item nav-link">Blazers</a>
-                    <a href="" class="nav-item nav-link">Jackets</a>
-                    <a href="" class="nav-item nav-link">Shoes</a>
-                </div>
-            </nav>
-        </div>
-        <div class="col-lg-9">
-            <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
-                <a href="" class="text-decoration-none d-block d-lg-none">
-                    <h1 class="m-0 display-5 font-weight-semi-bold"><span
-                            class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
-                </a>
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                    @include('web2.layout.menu')
 
-
-                    <div class="navbar-nav ml-auto py-0">
-
-
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
-</div>
 <!-- Navbar End -->
 
 
@@ -66,8 +13,8 @@
             <div class="card p-3">
                 <div class="text-center">
                     <img src="{{ Auth::user()->avatar ? asset('storage/avatars/' . Auth::user()->avatar) : asset('images/anhlogin.jpg') }}"
-                    width="50" height="50" class="rounded-circle border" style="object-fit: cover;">
-                
+                        width="50" height="50" class="rounded-circle border" style="object-fit: cover;">
+
                     <p>Xin chào</p>
                     <h5 class="mt-2"><strong>{{ Auth::user()->name }}</strong></h5>
                     <p class="text-muted">Member</p>
@@ -94,7 +41,7 @@
             <div class="card p-4">
                 <h4 class="text-center">THÔNG TIN TÀI KHOẢN</h4>
                 <div class="text-center">
-                   <img src="{{ Auth::user()->avatar ? asset('storage/avatars/' . Auth::user()->avatar) : asset('images/anhlogin.jpg') }}"
+                    <img src="{{ Auth::user()->avatar ? asset('storage/avatars/' . Auth::user()->avatar) : asset('images/anhlogin.jpg') }}"
                         width="120" height="120" class="rounded-circle border" style="object-fit: cover;">
 
                 </div>
@@ -112,7 +59,7 @@
                     </div>
 
                     <div class="row mt-3">
-                        <div class="col-md-6">
+                        <div class="col-md-7">
                             <label><strong>Email</strong></label>
                             <div class="input-group">
                                 <input type="text" class="form-control" value="{{ Auth::user()->email }}" disabled>
