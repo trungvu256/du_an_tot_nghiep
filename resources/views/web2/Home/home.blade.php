220b9adce248a3756f99edd455b3c6e8439ab461
trongvnph47351 <vuongngoctrong1703@gmail.com>
2025-03-27 11:12:33 +0700

    menu_client

--- resources/views/web2/Home/home.blade.php
+++ resources/views/web2/Home/home.blade.php
@@ -54,25 +54,7 @@ body {
     <div class="row border-top px-xl-5">
 
         <div class="container-fluid">
-            <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
-                <a href="/" class="text-decoration-none d-block d-lg-none">
-                    <h1 class="m-0 display-5 font-weight-semi-bold"><span
-                            class="text-primary font-weight-bold border px-3 mr-1">Ethereal</span>Noir</h1>
-                </a>
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                    {{-- @include('web2.layout.menu') --}}
 
-                    <div class="navbar-nav ml-auto py-0">
-
-                    </div>
-                    <div class="navbar-nav ml-auto py-0">
-
-                    </div>
-                </div>
-            </nav>
             <div id="header-carousel" class="containerfluid carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" style="height: 550px;">
