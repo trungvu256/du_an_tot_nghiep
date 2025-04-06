4b46c404c82fc1cc0969c663be9e6c7ae767e8bc
trongvnph47351 <vuongngoctrong1703@gmail.com>
2025-04-07 02:31:45 +0700

    giaodien2_v3

--- resources/views/web3/layout/header.blade.php
+++ resources/views/web3/layout/header.blade.php
@@ -1,5 +1,16 @@
 <body>
+    <!-- Scroll Top -->
+    <button id="goTop">
+        <span class="border-progress"></span>
+        <span class="icon icon-arrow-right"></span>
+    </button>
 
+    <!-- preload -->
+    <div class="preload preload-container">
+        <div class="preload-logo">
+            <div class="spinner"></div>
+        </div>
+    </div>
     <div id="wrapper">
         <header id="header" class="header-default header-search header-absolute-2 header-uppercase">
             <div class="header-top">
