abd62440cf9f0c977838e003a7266ed1fce689ba
trongvnph47351 <vuongngoctrong1703@gmail.com>
2025-04-07 22:29:26 +0700

    giaodien_end

---
+++ resources/views/web3/Home/dieukhoan.blade.php
@@ -0,0 +1,152 @@
+@extends('web3.layout.master2')
+@section('content')
+
+<body>
+    <div id="wrapper">
+
+
+        <div class="tf-breadcrumb">
+            <div class="container">
+                <ul class="breadcrumb-list">
+                    <li class="item-breadcrumb">
+                        <a href="index-2.html" class="text">Home</a>
+                    </li>
+                    <li class="item-breadcrumb dot">
+                        <span></span>
+                    </li>
+                    <li class="item-breadcrumb">
+                        <span class="text">Điều khoản và điều kiện</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /Breadcrumb -->
+
+
+        <!-- Title Page -->
+        <section class="s-title-page">
+            <div class="container">
+                <h4 class="s-title letter-0 text-center">
+                    Terms & Conditions
+                </h4>
+            </div>
+
+
+        </section>
+        <!-- /Title Page -->
+
+        <!-- Term -->
+        <section class="s-term-user flat-spacing-25">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="content">
+                            <div class="term-item">
+                                <p class="term-title">
+                                    Bản quyền và Thương hiệu
+                                </p>
+                                <p class="term-text">
+                                    Trừ khi có quy định khác, tất cả tài liệu trên trang web này, bao gồm nhưng không
+                                    giới hạn ở văn bản, hình ảnh, minh họa, phần mềm, đoạn âm thanh, đoạn video, tệp
+                                    hoạt họa, đều thuộc bản quyền và quyền thương hiệu của Công ty TNHH. Do đó, tài liệu
+                                    trên trang web này không được sao chép, tái sản xuất, chỉnh sửa, đăng tải, truyền
+                                    tải, phân phối, toàn bộ hoặc một phần dưới bất kỳ hình thức nào, nếu không có sự
+                                    đồng ý bằng văn bản trước của Công ty TNHH. Mọi quyền được bảo lưu.
+                                </p>
+                            </div>
+                            <div class="term-item">
+                                <p class="term-title">
+                                    Sản phẩm, Nội dung và Thông số kỹ thuật
+                                </p>
+                                <p class="term-text">
+                                    Tất cả tính năng, nội dung, thông số kỹ thuật, sản phẩm và giá cả của sản phẩm và
+                                    dịch vụ được mô tả hoặc hiển thị trên trang web này có thể thay đổi bất kỳ lúc nào
+                                    mà không cần thông báo trước. Một số trọng lượng, kích thước và mô tả tương tự chỉ
+                                    mang tính chất xấp xỉ và được cung cấp chỉ để tiện tham khảo. Chúng tôi nỗ lực hợp
+                                    lý để hiển thị chính xác các thuộc tính của sản phẩm, bao gồm cả màu sắc; tuy nhiên,
+                                    màu sắc thực tế bạn nhìn thấy phụ thuộc vào hệ thống máy tính của bạn và chúng tôi
+                                    không thể đảm bảo máy tính của bạn sẽ hiển thị chính xác những màu đó. Việc đưa vào
+                                    bất kỳ sản phẩm hoặc dịch vụ nào trên trang web này tại một thời điểm nhất định
+                                    không có nghĩa là những sản phẩm hoặc dịch vụ đó sẽ có sẵn bất kỳ lúc nào. Bạn có
+                                    trách nhiệm tuân thủ tất cả các luật địa phương, tiểu bang và quốc tế áp dụng liên
+                                    quan đến việc sở hữu, sử dụng và bán bất kỳ mặt hàng nào được mua từ trang web này.
+                                    Khi đặt hàng, bạn cam kết rằng sản phẩm được đặt sẽ chỉ được sử dụng một cách hợp
+                                    pháp.
+                                </p>
+                            </div>
+                            <div class="term-item">
+                                <p class="term-title">
+                                    Giới hạn vận chuyển
+                                </p>
+                                <p class="term-text">
+                                    Khi một đơn hàng được đặt, đơn hàng đó sẽ được gửi đến địa chỉ do người mua chỉ định
+                                    miễn là địa chỉ đó tuân thủ các giới hạn vận chuyển có trên trang web này. Tất cả
+                                    các giao dịch mua từ trang web này đều được thực hiện theo hợp đồng giao hàng. Do
+                                    đó, rủi ro mất mát và quyền sở hữu đối với các mặt hàng được mua từ trang web này sẽ
+                                    chuyển giao cho bạn khi mặt hàng được giao cho đơn vị vận chuyển. Bạn chịu trách
+                                    nhiệm nộp đơn khiếu nại với đơn vị vận chuyển nếu có hàng hóa bị hư hỏng và/hoặc
+                                    thất lạc.
+                                </p>
+                            </div>
+                            <div class="term-item">
+                                <p class="term-title">
+                                    Thuế và Phí
+                                </p>
+                                <p class="term-text">
+                                    Bạn chịu trách nhiệm cho tất cả các loại thuế và phí bên ngoài Singapore. Tất cả các
+                                    mặt hàng nhập khẩu vào quốc gia khác sẽ phải chịu kiểm tra hải quan và đánh giá thuế
+                                    và phí theo luật pháp quốc gia đó.
+                                </p>
+                            </div>
+                            <div class="term-item">
+                                <p class="term-title">
+                                    Tài khoản của bạn
+                                </p>
+                                <p class="term-text">
+                                    Bạn có trách nhiệm giữ bí mật thông tin tài khoản và mật khẩu của mình, và bạn đồng
+                                    ý chịu trách nhiệm cho tất cả các hoạt động diễn ra dưới tài khoản và mật khẩu đó.
+                                    Trang web này và Công ty TNHH có quyền từ chối cung cấp dịch vụ, chấm dứt tài khoản,
+                                    xóa hoặc chỉnh sửa nội dung hoặc hủy đơn hàng. Khi đặt hàng, bạn xác nhận rằng bạn
+                                    từ 18 tuổi trở lên và rằng bạn đang cung cấp cho shop.company.com hoặc
+                                    shop.beetle.com.sg thông tin chính xác, trung thực và rằng bạn có thẩm quyền để đặt
+                                    hàng.
+                                </p>
+                            </div>
+                            <div class="term-item">
+                                <p class="term-title">
+                                    Chính sách Đổi/Trả hàng (Cửa hàng Trực tuyến)
+                                </p>
+                                <div class="text-wrap">
+                                    <p class="term-text">
+                                        Giao tiếp điện tử
+                                    </p>
+                                    <p class="term-text">
+                                        Khi bạn truy cập trang web này và/hoặc gửi email cho chúng tôi, bạn đồng ý nhận
+                                        các thông tin liên lạc điện tử từ chúng tôi. Bạn đồng ý rằng tất cả các thỏa
+                                        thuận, thông báo, tiết lộ và các hình thức liên lạc khác mà chúng tôi gửi cho
+                                        bạn qua hình thức điện tử đều thỏa mãn yêu cầu pháp lý rằng các hình thức liên
+                                        lạc đó phải bằng văn bản.
+                                    </p>
+                                    <p class="term-text">
+                                        Miễn trừ bảo đảm
+                                    </p>
+                                    <p class="term-text">
+                                        Công ty TNHH không đưa ra bất kỳ đại diện hay bảo đảm nào về tình trạng hoạt
+                                        động hoặc chất lượng của trang web này, tính phù hợp để sử dụng, hoặc rằng việc
+                                        sử dụng nó sẽ không bị gián đoạn hay không có lỗi. Tất cả thông tin hoặc tài
+                                        liệu được cung cấp trên trang web này được cung cấp "nguyên trạng" và không có
+                                        bất kỳ bảo đảm nào, dù là rõ ràng hay ngụ ý, bao gồm nhưng không giới hạn, các
+                                        bảo đảm về khả năng thương mại, chất lượng, độ bền, tính phù hợp cho một mục
+                                        đích cụ thể, hoặc không vi phạm.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</body>
+@endsection
