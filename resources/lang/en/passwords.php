8ea383a33733884b935c968b85c666548a21cda3
trungvu256 <vutmph35719fpt@gmail.com>
2025-04-17 10:04:29 +0700

    update_regiter
    update_regiter

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,22 +1,21 @@
 <?php
-
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Dòng Ngôn Ngữ Đặt Lại Mật Khẩu
+|--------------------------------------------------------------------------
+|
+| Các dòng ngôn ngữ dưới đây là thông báo mặc định mà trình môi giới mật khẩu
+| trả về khi một yêu cầu cập nhật mật khẩu không thành công, như là do mã thông
+| báo không hợp lệ hoặc mật khẩu mới không hợp lệ.
+|
+*/
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+'reset' => 'Mật khẩu của bạn đã được đặt lại!',
+'sent' => 'Chúng tôi đã gửi liên kết đặt lại mật khẩu qua email của bạn!',
+'throttled' => 'Vui lòng chờ một chút trước khi thử lại.',
+'token' => 'Mã đặt lại mật khẩu này không hợp lệ.',
+'user' => 'Chúng tôi không tìm thấy người dùng với địa chỉ email này.',
 
 ];
